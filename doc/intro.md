ac02308ece4e452469e5468b13b3a58d2af2a319
Paul Gowder <paul.gowder@gmail.com>
2016-03-13 21:15:00 -0500

    initial

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to blabber
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/blabber/core_test.clj
@@ -0,0 +1,7 @@
+(ns blabber.core-test
+  (:require [clojure.test :refer :all]
+            [blabber.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
