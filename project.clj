dab5b3e67a09784427f6f8312d9e8880066288a4
Paul Gowder <paul.gowder@gmail.com>
2016-04-03 16:39:02 -0500

    small start on data handling functionality

--- project.clj
+++ project.clj
@@ -6,4 +6,5 @@
   :dependencies [[org.clojure/clojure "1.8.0"] 
                  [clj-fuzzy "0.1.8"]
                  [snowball-stemmer "0.1.0"]
-                 [net.mikera/core.matrix "0.50.0"]])
+                 [net.mikera/core.matrix "0.50.0"]
+		 [org.clojure/data.json "0.2.6"]])
--- src/blabber/core.clj
+++ src/blabber/core.clj
@@ -1,69 +1,88 @@
-(ns blabber.core 
-  (:require 
+(ns blabber.core
+  (:require
     [clojure.string :refer [lower-case split]]
     [clojure.walk :refer [keywordize-keys]]
-    [clojure.core.matrix.dataset :refer [dataset]]))
+    [clojure.core.matrix.dataset :refer [dataset]]
+    [clojure.data.json :as json]))
+
 
 (def tolower lower-case)
 
-(defn depunctuate 
+(defn depunctuate
   "strip punctuation from string"
-  [string] 
+  [string]
   (apply str (filter #(or (Character/isLetter %) (Character/isDigit %) (Character/isSpace %)) string)))
 
 (def digit? #(Character/isDigit %))
 
-(defn denumber 
+(defn denumber
   "strip numbers from string"
-  [string] 
+  [string]
   (apply str (filter #(not (digit? %)) string)))
 
 ; should probably just use remove for that rather than going wild to invert filter
 
-(defn default-preprocess 
-  "sensible default preprocessing" 
+(defn default-preprocess
+  "sensible default preprocessing"
   [string]
   (-> string depunctuate lower-case))
-(defn whitespace-split 
-  "split a vector of preprocessed strings into vector of vectors of strings on whitespace" 
-  [preprocessed-docs] 
+
+(defn whitespace-split
+  "split a vector of preprocessed strings into vector of vectors of strings on whitespace"
+  [preprocessed-docs]
   (pmap #(split % #"\s") preprocessed-docs))
-(defn count-strings 
-  "count frequencies of strings in vector of vectors of strings" 
+
+(defn count-strings
+  "count frequencies of strings in vector of vectors of strings"
   [stringvecs]
-  (pmap frequencies stringvecs))   
-(defn list-strings 
-  "list all strings in doc set" 
+  (pmap frequencies stringvecs))
+
+(defn list-strings
+  "list all strings in doc set"
   [stringvecs]
-  (distinct 
+  (distinct
     (apply concat stringvecs)))
-(defn cartesian-map 
-  [stringlist] 
+
+(defn cartesian-map
+  [stringlist]
   (zipmap stringlist (repeat 0)))
-(defn sparsify-counts 
+
+(defn sparsify-counts
   "based on strings in all preprocesed docs, fill counts with 0 for unused strings in each single preprocessed doc"
   [zeroes counts]
   (map #(merge-with + % zeroes) counts))
+
 (defn unsorted-TD-map
   "split vector of preprocessed docs by spaces then make zero-filled map of counts"
   [preprocessed-docs]
   (let [stringvecs (whitespace-split preprocessed-docs)]
-    (sparsify-counts 
-      (-> stringvecs list-strings cartesian-map) 
-      (-> stringvecs count-strings))))  
+    (sparsify-counts
+      (-> stringvecs list-strings cartesian-map)
+      (-> stringvecs count-strings))))
 
-(defn preprocessed-TD-matrix  
+(defn preprocessed-TD-matrix
   "make a core.matrix dataset from vector of preprocessed docs"
   [preprocessed-docs]
   (dataset (unsorted-TD-map preprocessed-docs)))
 
 (defn make-TD-matrix
   "preprocess docs then make term document matrix out of them"
-  ([docs] 
+  ([docs]
    (preprocessed-TD-matrix docs))
   ([docs & funcs]
    (let [preproc (apply comp funcs)]
      (preprocessed-TD-matrix (pmap preproc docs)))))
 
+(defn extract-texts
+  "map of docs and labels, like from json --> extract the docs. all labels assumed strings"
+  [docmap text-label]
+  (map #(get % text-label) docmap))
 
+; this is just some test code for json functionality. will go away soon.
+(def datarecs (json/read-str (slurp "test.json")))
+(extract-texts datarecs "text")
 
+; next steps:
+; 1. define a function to merge TD-mapped docs to labels.  probably should be able to take tf-idf'ed docs too.
+; 2. make something like make-TD-matrix-json that takes jsons instead of docs, pulls out the docs, processes, then merges
+; this will probably require decomposing the functionality in m-TD-m to not be repetitive
