eeec994651a50ee0ddfb0a097e681667e5aacadc
Paul Gowder <paul.gowder@gmail.com>
2016-05-26 20:51:52 -0500

    factor out core tdm functionality into more abstract featurizer

---
+++ src/MeowMeow/featurizer.clj
@@ -0,0 +1,48 @@
+(ns MeowMeow.featurizer
+  "Core abstraction: the featurizer. Idea is that you should be able to pass 
+  a vector of texts, and any function that produces a vector of features from 
+  text, to a higher-order function and that higher-order function should 
+  extract the features, count them, and return a universal map with counts across 
+  the entire dataset (vector of texts).  Also includes utility functionality for 
+  taking preexisting maps of text + feature and combining them, taking regexes 
+  and applying as featurizer functions, etc.")
+
+
+(defn- count-features
+  "count frequencies of features in vector of vectors of features"
+  [featurevecs]
+  (mapv frequencies featurevecs))
+
+(defn- list-features
+  "list all features in doc set"
+  [token-vecs]
+  (distinct
+    (apply concat token-vecs)))
+
+(defn- cartesian-map
+  [featurelist]
+  (zipmap featurelist (repeat 0)))
+
+(defn- sparsify-counts
+  "based on features in all preprocesed docs, fill counts with 0 for 
+  unused features in each single preprocessed doc"
+  [zeroes counts]
+  (mapv #(merge-with + % zeroes) counts))
+
+;; public functions follow
+
+(defn featurizer
+  "vector of documents + feature extracter function --> vector of maps with 
+  feature counts. Single-arity version is for convenience in tdm namespace"
+  ([vec-of-features feature-fn]
+    (let [feature-vecs (mapv feature-fn vec-of-features)]
+      (featurizer feature-vecs)))
+    ([vec-of-features]
+      (sparsify-counts
+        (-> vec-of-features list-features cartesian-map)
+        (-> vec-of-features count-features))))
+
+; TODO: fulfil the promise in the ns docfeature: "Also includes utility functionality for 
+;  taking preexisting maps of text + feature and combining them, taking regexes 
+;  and applying as featurizer functions, etc."
+
--- src/MeowMeow/tdm.clj
+++ src/MeowMeow/tdm.clj
@@ -9,40 +9,16 @@ The second, td-matrix takes a td-map and converts it to a single map:
 frequency vector-of-seqs is in standard term-document-matrix format (i.e., 
 documents on the rows, counts on the columns) and labels are keywords
 So obviously, the thing to do ordinarily is (td-matrix (td-map your-tokenized-dataset))
-This should all just pass seamlessly to any core.matrix flavor.")
+This should all just pass seamlessly to any core.matrix flavor."
+ (:require [MeowMeow.featurizer :as fea]))
 
-;; private functions
-
-(defn- count-strings
-  "count frequencies of strings in vector of vectors of strings"
-  [stringvecs]
-  (mapv frequencies stringvecs))
-
-(defn- list-strings
-  "list all strings in doc set"
-  [token-vecs]
-  (distinct
-    (apply concat token-vecs)))
-
-(defn- cartesian-map
-  [stringlist]
-  (zipmap stringlist (repeat 0)))
-
-(defn- sparsify-counts
-  "based on strings in all preprocesed docs, fill counts with 0 for unused strings in each single preprocessed doc"
-  [zeroes counts]
-  (mapv #(merge-with + % zeroes) counts))
-
-;; public functions follow
 
 (defn td-maps
   "vector of tokenized documents --> vector of maps with counts, including zeroes for terms 
   that do not appear in a given document.  Note that this is suitable for passing directly 
   into a core.mayrix dataset if that suits your fancy."
   [token-vecs]
-    (sparsify-counts
-      (-> token-vecs list-strings cartesian-map)
-      (-> token-vecs count-strings)))
+    (fea/featurizer token-vecs))
 
 (defn td-matrix
   "vector of matrices --> TDM proper as map w/ :labels :data.  N.B. to pass 
@@ -54,6 +30,3 @@ This should all just pass seamlessly to any core.matrix flavor.")
     {:labels (vec (keys (first smaps))) 
      :frequencies (mapv vals smaps)}))
 
-;; this needs a refactor.  New concept/abstraction: featurizer---function that 
-; gets features from documents.  Pass it to any seqable of documents and get back 
-; a map or matrix.  Not just tokenizers but also regex extraction and other parsers.
